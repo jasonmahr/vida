bde217309b339a793700df51fe2005567c4584f7
Ryoya Ogishima <ryoya9572ogishima@gmail.com>
2016-11-13 10:28:13 -0500

    Implement rating for iOS side

--- Vida/Vida/ListViewController.swift
+++ Vida/Vida/ListViewController.swift
@@ -13,6 +13,7 @@ class ListViewController: UIViewController {
     var i = 0
     let semaphore = DispatchSemaphore(value: 0)
     var json2 = JSON([])
+    var rate_score = 0
 
     @IBOutlet weak var name: UILabel!
     @IBOutlet weak var address: UILabel!
@@ -68,5 +69,59 @@ class ListViewController: UIViewController {
         self.male.text = json2[self.i]["male"].string
         self.female.text = json2[self.i]["female"].string
         i = (i+1)%(json2.count)
+    }
+    
+    func postAsync(sender: AnyObject) {
+        
+        // create the url-request
+        let urlString = "https://vida.herokuapp.com/api/rate"
+        let request = NSMutableURLRequest(url: NSURL(string: urlString)! as URL)
+        
+        // set the method(HTTP-POST)
+        request.httpMethod = "POST"
+        // set the header(s)
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        // prepare json data
+        let username_tmp = json2[self.i]["username"].string
+        print(username_tmp as Any)
+        let params = ["clubname": username_tmp as Any, "rating": rate_score, "comment": ""] as [String : Any]
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: params)
+        }catch _ as NSError{}
+        
+        // use NSURLSessionDataTask
+        let task = URLSession.shared.dataTask(with: request as URLRequest, completionHandler: {data, response, error in
+            if (error == nil) {
+                let result = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)!
+                print(result)
+                let json = JSON(data: data!)
+                print(json)
+            } else {
+                print(error as Any)
+            }
+        })
+        task.resume()
+    }
+    
+    @IBAction func rate1(_ sender: CustomButton) {
+        rate_score = 1
+        postAsync(sender: sender)
+    }
+    @IBAction func rate2(_ sender: CustomButton) {
+        rate_score = 2
+        postAsync(sender: sender)
+    }
+    @IBAction func rate3(_ sender: CustomButton) {
+        rate_score = 3
+        postAsync(sender: sender)
+    }
+    @IBAction func rate4(_ sender: CustomButton) {
+        rate_score = 4
+        postAsync(sender: sender)
+    }
+    @IBAction func rate5(_ sender: CustomButton) {
+        rate_score = 5
+        postAsync(sender: sender)
     }
 }
