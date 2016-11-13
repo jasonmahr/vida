1db2d923a42d63ae9f2d54218bcc6c996fd41e63
Ryoya Ogishima <ryoya9572ogishima@gmail.com>
2016-11-12 23:51:35 -0500

    Implement login page

--- Vida/Vida/MemberViewController.swift
+++ Vida/Vida/MemberViewController.swift
@@ -8,27 +8,27 @@
 
 import UIKit
 
-class MemberViewController: UIViewController {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.view.backgroundColor = UIColor.white
-        
-        // logout button
-        var buttonLogout: UIButton = UIButton()
-        
-        buttonLogout.addTarget(self, action: Selector(("onButtonLogout:")), for: .touchUpInside)
-        self.view.addSubview(buttonLogout)
-}
-
-// logout transaction
-    func onButtonLogout(sender: UIButton) {
-        var ud = UserDefaults.standard
-        ud.removeObject(forKey: "isLogin")
-        
-        var appDelegate:AppDelegate = UIApplication.shared.delegate as! AppDelegate
-        appDelegate.window = UIWindow(frame: UIScreen.main.bounds)
-        appDelegate.window?.rootViewController = VisitorViewController()
-        appDelegate.window?.makeKeyAndVisible()
-    }
-}
+//class MemberViewController: UIViewController {
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        self.view.backgroundColor = UIColor.white
+//        
+//        // logout button
+//        var buttonLogout: UIButton = UIButton()
+//        
+//        buttonLogout.addTarget(self, action: Selector(("onButtonLogout:")), for: .touchUpInside)
+//        self.view.addSubview(buttonLogout)
+//}
+//
+//// logout transaction
+//    func onButtonLogout(sender: UIButton) {
+//        var ud = UserDefaults.standard
+//        ud.removeObject(forKey: "isLogin")
+//        
+//        var appDelegate:AppDelegate = UIApplication.shared.delegate as! AppDelegate
+//        appDelegate.window = UIWindow(frame: UIScreen.main.bounds)
+//        appDelegate.window?.rootViewController = VisitorViewController()
+//        appDelegate.window?.makeKeyAndVisible()
+//    }
+//}
